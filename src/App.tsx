641235d089401fdd9ee33dafc89d32b99ae3ff3d
Antoine MICHEL <antoine.michel.ext@up.coop>
2024-06-05 21:23:46 +0200

    add signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import './App.css';
+import SignIn from './Pages/Authenticate/SignIn';
+import { Typography } from '@mui/material';
 import SignUp from './Pages/Authenticate/SignUp';
 
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+        Griffon Consulting{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 function App() {
   return (
     <div>
       <header>
       </header>
       <SignUp />
+      <SignIn />
+      
+      <Copyright sx={{ mt: 8, mb: 4 }} />
     </div>
   );
 }
