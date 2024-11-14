92bc1f05024b85d650354021480c02c433725b1f
Antoine MICHEL <antoine.michel.ext@up.coop>
2024-11-14 10:33:20 +0100

    config

--- src/Pages/Layout/Header.tsx
+++ src/Pages/Layout/Header.tsx
@@ -67,9 +67,9 @@ function Header(): JSX.Element {
             }}>
             <Link to="/">
               <img
-                src={
-                  'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e6faf73568658154dae_SitemarkDefault.svg'
-                }
+                // src={
+                //   'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e6faf73568658154dae_SitemarkDefault.svg'
+                // }
                 style={logoStyle}
                 alt="logo"
               />
