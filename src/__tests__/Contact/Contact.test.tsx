668079072e53c90719e135ee2254f82c0fb11e75
GriffonConsulting <antoine.michel@griffonconsulting.fr>
2025-03-19 16:50:19 +0100

    add tests for contact page

---
+++ src/__tests__/Contact/Contact.test.tsx
@@ -0,0 +1,82 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CookiesProvider } from 'react-cookie';
+import Contact from '../../Pages/Contact/Contact';
+import i18n from '../../i18n';
+import api from '../../__generated__/api';
+import { mockNavigate } from '../../../setupTests';
+
+const theme = createTheme();
+
+describe('Contact Component', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  const renderSignUp = () =>
+    render(
+      <BrowserRouter>
+        <CookiesProvider>
+          <ThemeProvider theme={theme}>
+            <Contact />
+          </ThemeProvider>
+        </CookiesProvider>
+      </BrowserRouter>,
+    );
+
+  it('renders Contact form correctly', () => {
+    renderSignUp();
+    expect(screen.getByLabelText(`${i18n.t('email')} *`)).toBeInTheDocument();
+    expect(screen.getByLabelText(`${i18n.t('message')} *`)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: i18n.t('submit') })).toBeInTheDocument();
+  });
+
+  it('displays error when form is invalid', async () => {
+    renderSignUp();
+    fireEvent.change(screen.getByLabelText(`${i18n.t('email')} *`), {
+      target: { value: 'invalid-email' },
+    });
+    fireEvent.click(screen.getByRole('button', { name: i18n.t('submit') }));
+    await waitFor(() => expect(screen.getByText(i18n.t('emailError'))).toBeInTheDocument());
+    await waitFor(() => expect(screen.getByText(i18n.t('messageError'))).toBeInTheDocument());
+  });
+
+  it('calls API', async () => {
+    api.contact.contact = vi.fn((): any => Promise.resolve({ data: { result: 'fake-token' } }));
+    renderSignUp();
+
+    fireEvent.change(screen.getByLabelText(`${i18n.t('email')} *`), {
+      target: { value: 'test@example.com' },
+    });
+    fireEvent.change(screen.getByLabelText(`${i18n.t('message')} *`), {
+      target: { value: 'Valid@1234' },
+    });
+    fireEvent.click(screen.getByRole('button', { name: i18n.t('submit') }));
+
+    await waitFor(() => {
+      expect(api.contact.contact).toHaveBeenCalledWith({
+        email: 'test@example.com',
+        message: 'Valid@1234',
+      });
+    });
+  });
+
+  it('disable the button when fetching', async () => {
+    api.authenticate.signUp = vi.fn((): any => Promise.resolve({ data: { result: 'fake-token' } }));
+    renderSignUp();
+
+    fireEvent.change(screen.getByLabelText(`${i18n.t('email')} *`), {
+      target: { value: 'test@example.com' },
+    });
+    fireEvent.change(screen.getByLabelText(`${i18n.t('message')} *`), {
+      target: { value: 'Valid@1234' },
+    });
+    fireEvent.click(screen.getByRole('button', { name: i18n.t('submit') }));
+
+    await waitFor(() => {
+      expect(screen.getByRole('button', { name: i18n.t('submit') })).toBeDisabled();
+    });
+  });
+});
